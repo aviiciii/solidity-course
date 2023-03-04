569ce799c288941f24886060927f08ed101c1569
aviiciii <laavesh1@gmail.com>
2023-03-04 16:19:44 +0530

    import chainlink price feed

--- lesson 3/FundMe.sol
+++ lesson 3/FundMe.sol
@@ -2,6 +2,10 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
+// Eth to usd convertor
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+
 contract FundMe{
 
     // saves the address of the person who sent the money and the amount of money they sent
@@ -10,6 +14,12 @@ contract FundMe{
     // function to send money to the contract
     function fund() public payable{
            transactions[msg.sender] += msg.value;
+    }
+
+    function get_version() public view returns(uint256){
+        // get eth/usd address for goerli from: https://docs.chain.link/data-feeds/price-feeds/addresses
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return pricefeed.version();
     }
 
 }
